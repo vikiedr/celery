68e08effc019a0e1402b14f320b2820e9b281656
vaclav.mracek <mracekv@gmail.com>
2023-11-01 15:43:12 +0100

    ss

--- celery.py
+++ celery.py
@@ -6,5 +6,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alzcript.settings')
 
 app = Celery('alzcript')
 
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.broker_url = 'amqp://vikiedr:bb4c2d870181d2b4fc6de2c1989295ba@rabbitmq-t6uy:5672'  # TODO z env
 app.autodiscover_tasks()
