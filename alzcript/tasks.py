252010f960f18151a5ca1ecda688977133fbaf56
vaclav.mracek <mracekv@gmail.com>
2023-11-02 09:24:10 +0100

    s

--- alzcript/tasks.py
+++ alzcript/tasks.py
@@ -3,6 +3,6 @@ from celery import Celery, shared_task
 app = Celery('alzcript')
 app.conf.broker_url = 'amqp://vikiedr:bb4c2d870181d2b4fc6de2c1989295ba@rabbitmq-t6uy:5672'
 
-@shared_task(name="test")
+@app.task(name='test')  # Named task
 def test(self):
     print('Testing')
