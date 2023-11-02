8fba54486bfb7c79ff7c0725aedf45bc67bebc98
vaclav.mracek <mracekv@gmail.com>
2023-11-02 10:20:27 +0100

    as

--- alzcript/celery.py
+++ alzcript/celery.py
@@ -8,3 +8,7 @@ app = Celery('alzcript')
 
 app.conf.broker_url = 'amqp://vikiedr:bb4c2d870181d2b4fc6de2c1989295ba@rabbitmq-t6uy:5672'  # TODO z env
 app.autodiscover_tasks()
+
+@app.task(name='test')  # Named task
+def test():
+    print('Testing')
